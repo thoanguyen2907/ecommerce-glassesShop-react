968b84c7917e278d35742da5969b739cff2f5829
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-01-05 14:05:49 +0200

    edit domainn link to localhsot

--- front-end/src/utils/constants/settingSystem.tsx
+++ front-end/src/utils/constants/settingSystem.tsx
@@ -1,6 +1,6 @@
-export const DOMAIN = 'https://ecommerece-thoa-nguyen.herokuapp.com/api/v1'
+export const DOMAIN = 'http://localhost:5000/api/v1/'
 
-export const DOMAIN_USER_AUTH = 'https://ecommerece-thoa-nguyen.herokuapp.com/api/v1/auth'
+export const DOMAIN_USER_AUTH = 'http://localhost:5000/api/v1/auth'
 
 export const STATUSCODE = {
   SUCCESS: 200,
