9c99a3703b1e06840fbfa76919f6fdf71e37e9b3
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-01-12 11:20:48 +0200

    add product infor

--- front-end/src/pages/TryOnGlasses/TryOnGlasses.tsx
+++ front-end/src/pages/TryOnGlasses/TryOnGlasses.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { ButtonAddToCart } from '../../StyledElements/ButtonAddToCart/ButtonAddToCart'
 import { Container } from '../../StyledElements/Container/Container'
 import { ValueSearch } from '../../types'
+import { truncateString } from '../../utils/truncateString/truncateString'
 import './TryOnGlasses.scss'
 
 
@@ -12,6 +15,14 @@ export default function TryOnGlasses() {
   const [glasses, setGlasses] = useState({
     display: false,
     chosenGlasses: ''
+  })
+  const [productChosen, setProductChosen] = useState({
+    display: false, 
+    description: '',
+    name: '',
+    price: '',
+    brand: '',
+    _id: ''
   })
   const valueSearch: ValueSearch = {
     brand: '',
@@ -41,14 +52,21 @@ export default function TryOnGlasses() {
       return   <div className="col-4 d-flex align-items-center" key = {index}>
          <img src={item.virtualImg} onClick = {() => {
            setGlasses({...glasses, chosenGlasses: item.virtualImg, display: true})
-
+           setProductChosen({...productChosen, 
+            brand: item.brand,
+            price: item.price,
+            description: item.description,
+            name: item.name,
+            _id: item._id,
+            display: true})
          }} key ={index} alt= {item.virtualImg} className='img-fluid vglasses__items my-3'  />
       </div>
       
     })
   }
   const displayClass = glasses.display? 'd-block' : 'd-none'
-  
+
+  const displayInfo = productChosen.display? 'd-block' : 'd-none'
     return (
       <Container>
        <div className="vglasses py-3">
@@ -68,15 +86,27 @@ export default function TryOnGlasses() {
         <div className="mb-2 text-right mt-2 mr-2">
           <button className="btn-before" onClick = {()=> {
             setGlasses({...glasses, display: false})
+            setProductChosen({...productChosen, display: false})
           }}>Before</button>
           <button className="btn-after" onClick = {()=> {
             setGlasses({...glasses, display: true})
+            setProductChosen({...productChosen, display: true})
           }}>After</button>
         </div>
         <div className="vglasses__model" id="avatar">
         <img src={glasses.chosenGlasses} className = {displayClass} id="glasses" alt='chosenGlasses'/>
         </div>
-        <div id="glassesInfo" className="vglasses__info">
+        <div id="glassesInfo" className= {`vglasses__info ${displayInfo}`}>
+            <h6 className='text-white'> {productChosen.brand} </h6>
+            <h6 className='text-white'>{productChosen.name}</h6>
+            <p className="card-text">
+            <span className="btn btn-success btn-sm mr-2"> $ {productChosen.price}</span>
+            </p>
+            <p className="card-text"> {truncateString(productChosen.description, 100)}  </p>
+            <NavLink to = {`/products/${productChosen._id}`}>
+           <ButtonAddToCart>  
+              Product Detail
+       </ButtonAddToCart></NavLink> 
         </div>
       </div>
     </div>
